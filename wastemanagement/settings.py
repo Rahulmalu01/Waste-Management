8a07909fb44b502afd99c9e41137df8e1b88bd59
rahulmalu01 <rahulmalu25@gmail.com>
2026-03-25 17:10:24 +0530

    security

--- wastemanagement/settings.py
+++ wastemanagement/settings.py
@@ -155,8 +155,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-TELEGRAM_BOT_TOKEN = os.getenv("")
-TELEGRAM_CHAT_ID = os.getenv("")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 OFFLINE_THRESHOLD_MINUTES = 15
 
@@ -164,8 +164,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("")
-EMAIL_HOST_PASSWORD = os.getenv("")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 MANAGER_REPORT_EMAILS = [
